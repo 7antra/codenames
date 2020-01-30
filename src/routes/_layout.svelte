2728a0acc44c4d51cd298a8c0cb6d2d950a5d836
7antra <yann@boug.art>
2020-01-30 19:22:29 +0100

    mobile détection

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,6 +1,10 @@
 <script>
+	import { onMount } from "svelte";
+	import { mobile } from "../store";
 	import Nav from "../components/Nav.svelte";
 	export let segment;
+
+	onMount(() => mobile.set(window.matchMedia("(max-width: 700px)").matches));
 </script>
 
 <style lang="scss" global>
